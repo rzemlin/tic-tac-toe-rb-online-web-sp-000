21b288972f4af41ae8a47206990ae22694473b31
Ryan Zemlin <rzemlin@gmail.com>
2021-01-24 19:55:00 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,4 +33,10 @@ def position_taken?(board, index)
   end
 end
 
-
+def valid_move?(board, index)
+  if !position_taken?(board, index) && (index).between?(0,8)
+    return true
+  else 
+    return false
+  end
+end
