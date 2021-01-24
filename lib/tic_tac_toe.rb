9d66f3cea6cf2d07461aa5f66b18a84a422e8527
Ryan Zemlin <rzemlin@gmail.com>
2021-01-24 20:00:38 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,3 +50,16 @@ def turn_count(board)
   }
    counter
 end
+
+def turn_count(board)
+  # board.count{|token| token == "X" || token == "O"}
+
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "O"
+      turns += 1
+    end
+  end
+  turns
+
+
